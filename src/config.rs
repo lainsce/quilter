e8bd0bef5ff661a49368bc01e73d97a78e321f90
Lains <lainsce@airmail.cc>
2021-01-10 04:45:01 -0300

    *

---
+++ src/config.rs
@@ -0,0 +1,4 @@
+pub static APP_ID: &str = "com.github.lainsce.quilter";
+pub static GETTEXT_PACKAGE: &str = "quilter";
+pub static LOCALEDIR: &str = "/app/share/locale";
+pub static PROFILE: &str = "";
